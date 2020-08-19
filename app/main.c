2ca80cae89fa14554d52bfe089f5c53a2bf680bf
dingjingmaster <dingjing@kylinos.cn>
2020-08-19 11:05:00 +0800

    main

---
+++ app/main.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+> FileName: main.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: 2020年08月19日 星期三 10时50分36秒
+ ************************************************************************/
+
+#include <glib.h>
+
+static GOptionEntry entries[] = {
+    {NULL}
+};
+
+int main (int argc, char* argv[])
+{
+
+    return 0;
+}
