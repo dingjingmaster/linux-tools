60e0ac54b96de838f012d619302bdd225e9bc13c
dingjingmaster <dingjing@live.cn>
2020-08-19 15:52:44 +0800

    main

--- app/main.c
+++ app/main.c
@@ -6,13 +6,49 @@
  ************************************************************************/
 
 #include <glib.h>
+#include <syslog/clib_syslog.h>
+
+#define APP_NAME "graceful-linux-command-tools"
+
+typedef struct _GlobalInput {
+    gboolean        appAll;
+    char*           appName;
+} GlobalInput;
+
+static void run_command (FlobalInput* app);
+
+static GlobalInput app;
 
 static GOptionEntry entries[] = {
+    {"app-all", 0, 0, G_OPTION_ARG_NONE, &(app.appAll), "List all installed application", NULL},
+    {"app", 0, 0, G_OPTION_ARG_STRING, &(app.appName), "Query the app information based on the .desktop name", "[desktop name]"},
     {NULL}
 };
 
 int main (int argc, char* argv[])
 {
+    GError* error = NULL;
+    GOptionContext* context = NULL;
+
+    context = g_option_context_new (APP_NAME);
+    if (NULL == context) {
+        CT_SYSLOG(LOG_ERR, "");
+        exit(-1);
+    }
+
+    g_option_context_add_main_entries (context, entries, NULL);
+
+    if (!g_option_context_parse(context, &argc, &argv, &error)) {
+        CT_SYSLOG(LOG_ERR, "option parsing failed:%s", error->message);
+        exit(-1);
+    }
+
+    run_command (context);
 
     return 0;
+}
+
+static void run_command (FlobalInput* app)
+{
+
 }
